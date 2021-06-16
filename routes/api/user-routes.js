94c242d30bdb0bc31b3638a8386469318c07714f
ariellongoria <ariello97@hotmail.com>
2021-06-16 18:09:16 -0500

    poggers;

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -45,14 +45,37 @@ router.post("/", (req, res) => {
         });
 });
 
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email,
+        },
+    }).then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user with that email address!" });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect Password" });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: "You are now logged in" });
+    });
+});
+
 router.put("/:id", (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id,
         },
     })
         .then((dbUserData) => {
-            if (!dbUSerData[0]) {
+            if (!dbUserData[0]) {
                 res.status(404).json({ nessage: "No user found with this id" });
                 return;
             }
